829b8d61c6d69a1acf430b51b49137de6458fc66
Chris Hendrix <chendrix@pivotal.io>
2017-10-10 17:14:18 -0700

    Add run_cats
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- dotfiles/functions/cf.sh
+++ dotfiles/functions/cf.sh
@@ -10,6 +10,8 @@ CWD="$(dirname -- "${0}")"
 # shellcheck source=/dev/null
 source "${CWD}/capi.sh"
 # shellcheck source=/dev/null
+source "${CWD}/credhub.sh"
+# shellcheck source=/dev/null
 source "${CWD}/perm.sh"
 
 function deploy_cf() (
@@ -36,6 +38,76 @@ function deploy_cf() (
     -o "${PERM_CI_REPO}/cf-deployment-operations/add-perm.yml" \
     -o "${PERM_CI_REPO}/cf-deployment-operations/add-perm-monitor-api-sidecar.yml" \
     -o "${CAPI_CI_REPO}/cf-deployment-operations/use-latest-capi.yml"
+)
+
+function run_cats() (
+  set -eux
+
+  local admin_password
+  local admin_username
+  local config_file
+  local system_domain
+  local num_nodes
+
+  num_nodes="${NUM_NODES:-8}"
+  system_domain="${SYSTEM_DOMAIN:-"${BOSH_LITE_DOMAIN:-}"}"
+  GOPATH="${GOPATH_GLOBAL:-"${GOPATH}"}"
+
+  : "${system_domain:?"Need to set SYSTEM_DOMAIN or BOSH_LITE_DOMAIN"}"
+  : "${GOPATH:?"Need to set GOPATH or GOPATH_GLOBAL"}"
+
+  login_to_credhub
+
+  admin_password="$(credhub get -n /bosh-lite/cf/cf_admin_password --output-json)"
+  admin_password="$(echo "${admin_password}" | jq -r -e .value)"
+  admin_username="admin"
+
+  config_file="$(mktemp)"
+
+  export GOPATH="$GOPATH"
+  export CONFIG="$config_file"
+
+  go get -u -d github.com/cloudfoundry/cf-acceptance-tests
+
+  pushd "${GOPATH}/src/github.com/cloudfoundry/cf-acceptance-tests" > /dev/null
+    "${PWD}/bin/update_submodules"
+
+    cat > "$config_file" <<EOF
+{
+  "api": "api.${system_domain}",
+  "apps_domain": "${system_domain}",
+  "admin_user": "${admin_username}",
+  "admin_password": "${admin_password}",
+  "skip_ssl_validation": true,
+  "use_http": false,
+  "include_apps": true,
+  "include_backend_compatibility": false,
+  "include_capi_experimental": false,
+  "include_capi_no_bridge": false,
+  "include_container_networking": false,
+  "include_credhub" : false,
+  "include_detect": false,
+  "include_docker": false,
+  "include_internet_dependent": false,
+  "include_isolation_segments": false,
+  "include_persistent_app": false,
+  "include_private_docker_registry": false,
+  "include_privileged_container_support": false,
+  "include_route_services": false,
+  "include_routing": true,
+  "include_routing_isolation_segments": false,
+  "include_security_groups": false,
+  "include_services": true,
+  "include_ssh": false,
+  "include_sso": false,
+  "include_tasks": false,
+  "include_v3": true,
+  "include_zipkin": false
+}
+EOF
+
+    "${PWD}/bin/test" -nodes="$num_nodes"
+  popd > /dev/null
 )
 
 function upload_releases_and_deploy_cf() (
