6e1dee33aa03d1dd14a03adf06641c06658fdac9
Chris Hendrix <chendrix@pivotal.io>
2017-10-16 14:47:57 -0700

    Add target_ci_bosh_lite
    [Finishes #152016474]
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- dotfiles/functions/credhub.sh
+++ dotfiles/functions/credhub.sh
@@ -3,15 +3,27 @@
 function login_to_credhub() (
   set -eu
 
+  CREDHUB_CA_CERT="${CREDHUB_CA_CERT:-}"
+
   : "${CREDHUB_SERVER:?"Need to set CREDHUB_SERVER"}"
 
+  if [[ -z "$CREDHUB_CA_CERT" ]]; then
+    SKIP_SSL_VALIDATION="true"
+  fi
+
   if [[ -n "${CREDHUB_CLIENT:-}" && -n "${CREDHUB_SECRET:-}" ]]; then
-    credhub login --skip-tls-validation
+    credhub login \
+      "${CREDHUB_SERVER:+--server}" \
+      "${CREDHUB_SERVER:+"${CREDHUB_SERVER}"}" \
+      "${SKIP_SSL_VALIDATION:+--skip-tls-validation}"
   else
     : "${CREDHUB_USERNAME:?"Need to set CREDHUB_USERNAME"}"
     : "${CREDHUB_PASSWORD:?"Need to set CREDHUB_PASSWORD"}"
 
-    credhub login --skip-tls-validation \
+    credhub login \
+      "${SKIP_SSL_VALIDATION:+--skip-tls-validation}" \
+      "${CREDHUB_SERVER:+--server}" \
+      "${CREDHUB_SERVER:+"${CREDHUB_SERVER}"}" \
       --username "$CREDHUB_USERNAME" \
       --password "$CREDHUB_PASSWORD"
   fi
